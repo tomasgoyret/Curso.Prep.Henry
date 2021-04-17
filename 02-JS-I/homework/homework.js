cd17286ad7fa5db3361198a7dd25c445cd10b87c
Tomás Goyret <tomas.goyret@gmail.com>
2021-04-17 16:26:58 -0300

    vercorrectafinal

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -233,7 +233,7 @@ function esVocal(letra){
   if (letra.length > 1){
     return "Dato incorrecto";
   }
-  else if (letra=="a","e","i","o","u") {
+  else if (letra==="a"  || letra==="e" || letra==="i"  ||letra==="o"||letra==="u") {
     return "Es vocal";
   }
   return false;
